d430b460857dd6f1e530f356239c53a93d1bb96c
Matt Turner <git@mt165.co.uk>
2024-02-13 09:09:04 +0000

    Make run on Mac

---
+++ pkg/permissions/caps_macos.go
@@ -0,0 +1,16 @@
+package permissions
+
+import (
+	"github.com/go-logr/logr"
+)
+
+/* Lol capabilities
+* - TODO on mac?
+ */
+func ApplyNetRaw(log logr.Logger) {
+	log.Info("Caps no a thing on mac??")
+}
+
+func DropNetRaw(log logr.Logger) {
+	log.Info("Caps no a thing on mac??")
+}
